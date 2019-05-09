fb05113b117a42f84f9d359afdabe4c4ac1800df
Xh520630 <404213569@qq.com>
2019-05-09 11:32:42 +0800

    v1.3修改

--- test.php
+++ test.php
@@ -4,12 +4,13 @@ function a(){
     echo 123;
     echo 789;
     $a = '蔡徐坤';
-    if ($a == '周杰明')
-        echo '250';
+    if ($a == '周杰伦')
+        echo '帅逼';
 }
 
 function b(){
     echo 456;
     echo 000;
     echo '哈哈哈哈';
+    echo '我正在修改奇奇怪怪的东西'
 }
